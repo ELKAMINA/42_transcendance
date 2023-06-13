2b312c759e3ab493deb97de485c43e740bd7f336
Alicia <alicia.mrchd@gmail.com>
2023-06-13 18:12:20 +0200

    ADD: Provider & store tags to use redux

--- client/src/pages/chat.tsx
+++ client/src/pages/chat.tsx
@@ -1,14 +1,18 @@
+import { Provider } from 'react-redux';
 import Navbar from '../components/NavBar_0';
 import SideBar from './sideBar';
+import { store } from '../app/store';
 
 function Chat () {
 	const currentRoute = window.location.pathname;
 
 	return (
-		<div>
-			<Navbar currentRoute={ currentRoute }/>
-			<SideBar />
-		</div>
+		<Provider store={store}>
+			<div>
+				<Navbar currentRoute={ currentRoute }/>
+				<SideBar />
+			</div>
+		</Provider>
 	)
 }
 
