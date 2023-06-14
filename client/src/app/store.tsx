10ae657741bcdc5419d44336a2d4e9335dc177cb
Alicia <alicia.mrchd@gmail.com>
2023-06-14 12:01:05 +0200

    UPDT : channel becomes channels

--- client/src/app/store.tsx
+++ client/src/app/store.tsx
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import { apiSlice } from './api/apiSlice'
 import authReducer from '../features/auth/authSlice'
 import chatReducer from '../features/chat/chatSlice'
-import channelReducer from '../features/chat/channelSlice'
+import channelsReducer from '../features/chat/channelsSlice'
 import channelUserReducer from '../features/chat/channelUserSlice'
 import channelTypeReducer from '../features/chat/channelTypeSlice'
 import channelNameReducer from '../features/chat/channelNameSlice'
@@ -17,7 +17,7 @@ export const store = configureStore({
         // A standard redux reducer that enables core functionality
         auth: authReducer,
         chat: chatReducer,
-		channel: channelReducer,
+		channels: channelsReducer,
 		channelName: channelNameReducer,
 		channelUser: channelUserReducer,
 		channelType: channelTypeReducer,
