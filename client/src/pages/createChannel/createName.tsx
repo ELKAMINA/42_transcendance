ff25204a542b7ea5d02bc7c91ba48ca41f2e931a
Alicia <alicia.mrchd@gmail.com>
2023-06-15 12:46:23 +0200

    ADD: updated the channel name field, with check if name is taken

--- client/src/pages/createChannel/createName.tsx
+++ client/src/pages/createChannel/createName.tsx
@@ -1,36 +1,49 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./createChannel.css"
 import { RootState } from "../../app/store";
+import { TextField } from "@mui/material";
+import { useState } from "react";
+import { changeChannelName } from "../../features/chat/channelNameSlice";
 
 function CreateName() {
 	const channels = useSelector((state: RootState) => state.channels);
+	const [channelName, setChannelName] = useState('');
+	const [isTaken, setIsTaken] = useState(false);
 
 	const dispatch = useDispatch();
 
 	function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-		// FOR DEBUG
-		// console.log("new name is = ", e.target.value);
-		
-		const input = e.target.value;
-		if (channels.find(channel => channel.name === input))
-			alert("name is taken, please choose a different one");
-		else {
-			dispatch({
-				type: 'channelName/changeChannelName',
-				payload: input, 
-			});
-		}
+		setChannelName(e.target.value);
+
+		if (channels.find(channel => channel.name === channelName)) {
+			console.log("channelName taken")
+			setIsTaken(true);
+		} else {
+			console.log("channel name is available!")
+			dispatch(changeChannelName(channelName))
+		} 
 	}
 
 	return (
 	<div className='entry1'>
 		<label className='form-channel-name' htmlFor='channelName'>channel name</label>
 		<br></br>
-		<input 
-			className='inputForm' 
-			type='text' 
-			name='channelName'
+		<TextField
+			type='channelName'
+			label='enter channel name'
+			variant = 'outlined'
+			color = 'success'
+			value = {channelName}
 			onChange={handleChange}
+			required
+			helperText={isTaken === true ? 'this name is taken!' : 'required'}
+			error={isTaken === true}
+			sx={{
+				'& input': {
+					backgroundColor: 'transparent',
+					boxShadow: 'none'
+				}
+			}}
 		/>
 	</div>
 	);
