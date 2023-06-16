4752f027e9b51fce3dc26f30543f6e631a733a81
Alicis Marchand <alicia.mrchd@gmail.com>
2023-06-16 12:46:37 +0200

    UPDT: interace is now a type + added profile_picture key in channel object

--- client/src/data/channelList.ts
+++ client/src/data/channelList.ts
@@ -1,18 +1,27 @@
 import { User } from "./userList";
+import chihiro from "../assets/profile_pictures/chihiro.jpeg"
+import totoro from "../assets/profile_pictures/totoro.jpeg"
 
-export interface Channel {
-	name: string;
-	id: number;
-	type: string;
+export type Channel = {
+
+	name: string
+	id: number
+	owner: string
+	profile_picture: string
+	type: string
 	protected_by_password: boolean
-	password: string,
+	password: string
 	userList: User[]
-  }
+
+}
+
 
 export const channelList:Channel[] = [
 	{
-		name: 'channel_0',
+		name: 'Chihiro',
 		id: 0,
+		owner: 'casper_le_fantome',
+		profile_picture: chihiro,
 		type: 'public',
 		protected_by_password: true,
 		password: 'kirikou',
@@ -20,8 +29,10 @@ export const channelList:Channel[] = [
 	},
 
 	{
-		name: 'channel_1',
+		name: 'Totoro',
 		id: 1,
+		owner: 'satsuki_3003',
+		profile_picture: totoro,
 		type: 'private',
 		protected_by_password: false,
 		password: 'kirikou',
