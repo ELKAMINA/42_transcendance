752708c8c50ca1cab68aaf3c937084f5d224e641
Alicia Marchand <alicia.mrchd@gmail.com>
2023-06-19 15:50:22 +0200

    ADD: starting to add messages windows

--- client/src/pages/chat.tsx
+++ client/src/pages/chat.tsx
@@ -2,16 +2,41 @@ import { Provider } from 'react-redux';
 import Navbar from '../components/NavBar';
 import SideBar from './sideBar';
 import { store } from '../app/store';
+import { Box, Grid } from '@mui/material';
 
 function Chat () {
 	const currentRoute = window.location.pathname;
 
 	return (
 		<Provider store={store}>
-			<div>
+			<Grid container sx={{ height: '100vh' }}>
+				<Grid item xs={12}>
+					<Navbar currentRoute={currentRoute} />
+				</Grid>
+				<Grid item>
+					<SideBar />
+				</Grid>
+				<Grid item xs={true}>
+					<Box
+						sx={{
+						backgroundColor: '#ffff',
+						height: '100%',
+						}}
+					></Box>
+				</Grid>
+			</Grid>
+			{/* <Box>
 				<Navbar currentRoute={ currentRoute }/>
 				<SideBar />
-			</div>
+				<Box
+					sx={{
+						height:'100%'
+						width:''
+						backgroundColor: '#ffff'
+					}} 
+				>
+				</Box>
+			</Box> */}
 		</Provider>
 	)
 }
