3aaf003dbbba70d2a334333033256f64b861f5db
Alicia Marchand <alicia.mrchd@gmail.com>
2023-06-20 15:12:02 +0200

    UPDT: added divider, change button color and moved sx in css file

--- client/src/pages/sideBar.tsx
+++ client/src/pages/sideBar.tsx
@@ -8,8 +8,8 @@ import "./sideBar.css"
 import { useSelector } from 'react-redux';
 import { RootState } from '../app/store.tsx';
 import { UserDetails } from "../../../server/src/user/types/user-types.user.ts";
-import AlignItemsList from '../components/ChannelDisplayList.tsx';
-import { Box, Button, Grid, Stack } from '@mui/material';
+import AlignItemsList from '../components/AlignItemsList.tsx';
+import { Box, Button, Stack, Divider } from '@mui/material';
 
 interface Channel {
 	name: string;
@@ -44,14 +44,14 @@ function SideBar() {
 			<SearchBar content="Look for user, channel..." setResults={setResults} />
 			<SearchResultsList results={results}/>
 		</Stack>
-
+		<Divider variant='middle' flexItem  sx={{bgcolor: '#dde5ed'}}/>
 		<Stack className='createChannelButtonWrapper'>
 			<Button
 				onClick={() => setButtonPopup(true)}
 				variant='contained'
 				size='large'
 				sx={{
-					color: '#ac0404',
+					color: '#07457E',
 					backgroundColor: '#99E100',
 					fontWeight: '900',
 					fontSize: '1em',
@@ -66,27 +66,7 @@ function SideBar() {
 			<CreateChannel trigger = {buttonPopup} setTrigger={setButtonPopup} />
 		</Stack>
 
-		<Stack className='alignItemsListContainer' sx= {{
-			  overflowY: "auto", //  a scrollbar will appear when the content exceeds the element's height.
-			  margin: 0,
-			  padding: 0,
-			  listStyle: "none", //  Removes the default list-style (bullet points) from the element.
-			  height: "100%",
-			  '&::-webkit-scrollbar': { // Selects the scrollbar pseudo-element for webkit-based browsers (e.g., Chrome, Safari).
-				width: '0.5em', // Sets the width of the scrollbar to 0.4em.
-				border: 'none',
-
-			  },
-			  '&::-webkit-scrollbar-track': { //  Selects the track (background) of the scrollbar
-				boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)', // Applies an inset box shadow to the scrollbar track.
-				webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)' //  Applies an inset box shadow to the scrollbar track specifically for webkit-based browsers.
-			  },
-			  '&::-webkit-scrollbar-thumb': { //  Selects the thumb (scrolling handle) of the scrollbar.
-				backgroundColor: '#e6ebea', // Sets the background color of the scrollbar thumb to a semi-transparent black color.
-				borderRadius: '20px',
-				border: 'none',
-			  }
-		}}>
+		<Stack className='alignItemsListContainer'>
 			<AlignItemsList />
 		</Stack>
 	</Box>
