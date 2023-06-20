1425875d28241b85d36819e3bfdd85d8fb87d9c1
Alicia Marchand <alicia.mrchd@gmail.com>
2023-06-20 17:12:42 +0200

    FIX: newly created channel was not added to the list

--- client/src/components/AlignItemsList.tsx
+++ client/src/components/AlignItemsList.tsx
@@ -5,7 +5,6 @@ import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
 import { Channel } from '../data/channelList';
 import { useSelector } from 'react-redux';
 import { RootState } from '../app/store';
@@ -28,13 +27,11 @@ export default function AlignItemsList() {
 
 	// get list of users objects in a array
 	const allUsers: UserDetails[] = useAppSelector((state) => selectSuggestions(state) as UserDetails[]);
+	allUsers.map((user) => console.log('users = ', user));
 	
 	// create an array with channels and users object to display in local storage so that when I refresh the page, the
 	// update done to the state of combinedArray are still in memory.
-	const [combinedArray, setCombinedArray] = React.useState<combine[]>(() => {
-		const storedArray = localStorage.getItem('combinedArray');
-		return storedArray ? JSON.parse(storedArray) : [...channels, ...allUsers];
-	});
+	const [combinedArray, setCombinedArray] = React.useState<combine[]>([...channels, ...allUsers]);
 
 	// this function allow to remove channels or users from the displayed list
 	// it is triggered when the user click on the DeleteButton component
@@ -42,7 +39,6 @@ export default function AlignItemsList() {
 		setCombinedArray((prevArray) => {
 		  const newArray = [...prevArray];
 		  newArray.splice(index, 1); // Remove the item at the specified index
-		  localStorage.setItem('combinedArray', JSON.stringify(newArray)); // Store the updated array in localStorage
 		  return newArray;
 		});
 	}
@@ -51,45 +47,46 @@ export default function AlignItemsList() {
 	const [selectedIndex, setSelectedIndex] = React.useState(0);
 	const handleListItemClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>,
 		index: number, ) => {
+		console.log('combinedArray', combinedArray);
 		setSelectedIndex(index);
 	};
 	// --------------------------------------------------------------------------------
 	  
 	return (
-	<List sx={{ width: '100%', bgcolor: 'transparent', color: 'white' }}>
-		{combinedArray.map((element, index) => (
-			<Stack key={index}>
-				<Divider variant="inset" component="li" />
-				<ListItemButton
-					selected={selectedIndex === index}
-					onClick={(event) => handleListItemClick(event, index)}
-					sx={{
-						'&:hover': { backgroundColor: 'rgba(116, 131, 145, 0.4)' },
-						'&.Mui-selected': { backgroundColor: '#032B50' }
-
-					}}
-				>
-				<ListItem
-					alignItems="flex-start"
-					secondaryAction={
-						<div onClick={() => handleClick(index)}>
-							<IconButton aria-label="delete">
-							<DeleteIcon sx={{color:'red'}} fontSize='small'/>
-							</IconButton>
-						</div>
-					}
-				>
-					<ListItemAvatar>
-						<Avatar alt={element.login} src={element.avatar} />
-					</ListItemAvatar>
-					<ListItemText
-						sx= {{color:'white'}}
-						primary={element.login}
-					/>
-				</ListItem>
-				</ListItemButton>
-			</Stack>
-		))}
-	</List>
-	);
+		<List sx={{ width: '100%', bgcolor: 'transparent', color: 'white' }}>
+			{combinedArray.map((element, index) => (
+				<Stack key={index}>
+					<Divider variant="inset" component="li" />
+					<ListItemButton
+						selected={selectedIndex === index}
+						onClick={(event) => handleListItemClick(event, index)}
+						sx={{
+							'&:hover': { backgroundColor: 'rgba(116, 131, 145, 0.4)' },
+							'&.Mui-selected': { backgroundColor: '#032B50' }
+	
+						}}
+					>
+					<ListItem
+						alignItems="center"
+						secondaryAction={
+							<div onClick={() => handleClick(index)}>
+								<IconButton aria-label="delete">
+								<DeleteIcon sx={{color:'red'}} fontSize='small'/>
+								</IconButton>
+							</div>
+						}
+					>
+						<ListItemAvatar>
+							<Avatar alt={element.login} src={element.avatar} />
+						</ListItemAvatar>
+						<ListItemText
+							sx= {{color:'white'}}
+							primary={element.login}
+						/>
+					</ListItem>
+					</ListItemButton>
+				</Stack>
+			))}
+		</List>
+		);
 }
