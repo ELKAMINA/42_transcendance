43c2c2e0eb3dcabd4d4b5e3a9bb1a3ece2f34c26
Alicia Marchand <alicia.mrchd@gmail.com>
2023-06-20 15:12:48 +0200

    ADD: conversations

--- client/src/pages/chat.tsx
+++ client/src/pages/chat.tsx
@@ -2,30 +2,59 @@ import { Provider } from 'react-redux';
 import Navbar from '../components/NavBar';
 import SideBar from './sideBar';
 import { store } from '../app/store';
-import { Box, Grid } from '@mui/material';
+import { Box, Grid, Stack } from '@mui/material';
+import Conversation from '../components/Conversation';
+import './chat.css'
 
 function Chat () {
 	const currentRoute = window.location.pathname;
-
+  
 	return (
 		<Provider store={store}>
-			<Grid container sx={{ height: '100vh' }}>
-				<Grid item xs={12}>
-					<Navbar currentRoute={currentRoute} />
-				</Grid>
-				<Grid item>
-					<SideBar />
-				</Grid>
-				<Grid item xs={true}>
-					<Box
-						sx={{
-						backgroundColor: '#ffff',
-						height: '100%',
-						}}
-					></Box>
-				</Grid>
-			</Grid>
+			<Navbar currentRoute={ currentRoute }/>
+			<Stack className='chat' direction={'row'} sx={{width: '100%'}}>
+				<SideBar />
+				<Box sx={{
+					height: '95vh',
+					width: '100vw',
+					backgroundColor: '#fff',
+				}}>
+					<Conversation />
+				</Box>
+			</Stack>
 		</Provider>
+
+
+		// {/* <Box> */}
+		// {/* <Box */}
+		// 	// sx={{
+		// 		// height:'100%',
+		// 		// width:'75vw',
+		// 		// backgroundColor: 'black',
+		// 	// }}
+		// {/* > */}
+		// {/* </Box> */}
+		// {/* </Box> */}
+
+
+		// 	{/* <Grid container sx={{ height: '100vh' }}> */}
+		// 		{/* <Grid item xs={12}> */}
+		// 			{/* <Navbar currentRoute={currentRoute} /> */}
+		// 		{/* </Grid> */}
+		// 		{/* <Grid item> */}
+		// 			{/* <SideBar /> */}
+		// 		{/* </Grid> */}
+		// 		{/* <Grid item xs='auto' */}
+		// 			// sx={{
+		// 				// backgroundColor: 'black',
+		// 				// height: '100%',
+		// 				// width: '85vw',
+		// 				// }}
+		// 		// >
+		// 		{/* </Grid> */}
+		// 	{/* </Grid> */}
+		// {/* </Provider> */}
+
 	)
 }
 
