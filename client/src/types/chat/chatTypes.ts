cc9ac7c0c404aa8a7f04bcdf2e64afc3a8280c4e
Alicis Marchand <alicia.mrchd@gmail.com>
2023-06-22 18:15:07 +0200

    ADD: types

---
+++ client/src/types/chat/chatTypes.ts
@@ -0,0 +1,17 @@
+import { UserDetails } from "../users/userType"
+
+export type Channel = {
+
+	login: string
+	id: number
+	owner: string
+	avatar: string
+	type: string
+	protected_by_password: boolean
+	password: string
+	userList: UserDetails[]
+
+}
+
+// a chat is eather a one to one conversation (UserDetails) or a group conversation (Channel)
+export type Chat = UserDetails | Channel
---
+++ client/src/types/users/userType.ts
@@ -0,0 +1,7 @@
+export type UserDetails = {
+	login: string;
+	email: string;
+	displayName: string;
+	avatar: string;
+  };
+  
