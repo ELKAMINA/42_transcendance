7c2db3fa2ccfb6aa029ba76c1dd55521580941ba
Alicia Marchand <alicia.mrchd@gmail.com>
2023-06-26 16:26:17 +0200

    FIX: added a check to make sure creator exist in database, connected members to User and createdBy (aka owner) to Users element of type model User + typed data as Prisma.ChannelCreateInput

--- server/src/channel/channel.service.ts
+++ server/src/channel/channel.service.ts
@@ -1,4 +1,4 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { ChannelDto } from './dto/channelPayload.dto';
 import * as argon from 'argon2';
@@ -13,22 +13,30 @@ export class ChannelService {
 		console.log("coucouuuuu");
 		const pwd = await argon.hash(dto.password);
 		try {
+
+			// before creating the Channel record, 
+			// we fetch the User record with the specified login value using this.prisma.user.findUnique(). 
+			// If the creator record is not found, 
+			// we throw a NotFoundException with an appropriate error message.
+			const creator = await this.prisma.user.findUnique({
+				where: { login: dto.owner },
+			});
+			console.log('creator = ', creator);
+			if (!creator) {
+				throw new NotFoundException(`User with login '${dto.owner}' not found.`);
+			}
+
+			// we create channel record
 			const channel = await this.prisma.channel.create({
 				data: {
 					name: dto.login,
-					members: {
-						createMany: {
-							data: dto.userList,
-						},
-					},
+					members: {connect: dto.userList.map((user) => ({ login: user.login })),},
 					createdBy: {
-						connect: {
-							user_id: dto.owner,
-						},
+						connect: {login: dto.owner}
 					},
 					type: dto.type,
-					key: pwd,
-				},
+					key: dto.password,
+				} as Prisma.ChannelCreateInput,
 			});
 		return channel;
 		} catch (error: any) {
