407368942285adf161f941fba42d4458ccda38c0
Alicia Marchand <alicia.mrchd@gmail.com>
2023-06-26 14:26:24 +0200

    ADD: service to create new channel entry in database

--- server/src/channel/channel.service.ts
+++ server/src/channel/channel.service.ts
@@ -1,9 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { ChannelDto } from './dto/channelPayload.dto';
+import * as argon from 'argon2';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class ChannelService {
-  constructor(private prisma: PrismaService) {}
+	constructor(
+		private prisma: PrismaService) {}
 
-
+	async createChannel(dto: ChannelDto): Promise<object> {
+		console.log("coucouuuuu");
+		const pwd = await argon.hash(dto.password);
+		try {
+			const channel = await this.prisma.channel.create({
+				data: {
+					name: dto.login,
+					members: {
+						createMany: {
+							data: dto.userList,
+						},
+					},
+					createdBy: {
+						connect: {
+							user_id: dto.owner,
+						},
+					},
+					type: dto.type,
+					key: pwd,
+				},
+			});
+		return channel;
+		} catch (error: any) {
+			if (error instanceof Prisma.PrismaClientKnownRequestError) {
+				if (error.code === 'P2002') {
+				  throw new ForbiddenException('Credentials taken');
+				}
+			  }
+			  throw error;
+		}
+	}
 }
+
