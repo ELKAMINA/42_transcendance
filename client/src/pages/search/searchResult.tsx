625390a84a8550d8d3d89b5ebc5082feea9f31b1
Alicia Marchand <alicia.mrchd@gmail.com>
2023-06-28 15:23:09 +0200

    ADD: established road map

--- client/src/pages/search/searchResult.tsx
+++ client/src/pages/search/searchResult.tsx
@@ -1,12 +1,21 @@
+import { Box } from "@mui/material";
 import "./searchResult.css";
 
 export const SearchResult = ({ result }: { result: string }) => {
-  return (
-    <div
-      className="search-result"
-      onClick={() => alert(`You selected ${result}!`)}
-    >
-      {result}
-    </div>
-  );
+
+	// if result is a channel that <login> is a creator/member, display message component
+	// if result is a public channel not protected by password, display message component
+	// if result is a public channel protected by a password, ask for password than display message component
+
+	// if result is a user that already has an non-empty message_history  with <login>, open message component
+	// if result is a user that has an empty message_history with <login>, open user profile
+	
+	return (
+		<Box
+		className="search-result"
+		onClick={() => alert(`You selected ${result}!`)}
+		>
+		{result}
+		</Box>
+	);
 };
