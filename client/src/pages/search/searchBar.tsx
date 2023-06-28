ec7f9ac1a118635cebc57a96b3f31ef85e4818cf
Alicia Marchand <alicia.mrchd@gmail.com>
2023-06-28 13:30:26 +0200

    UPDT: results are now user AND channels

--- client/src/pages/search/searchBar.tsx
+++ client/src/pages/search/searchBar.tsx
@@ -6,8 +6,10 @@ import { FetchAllUsers, selectSuggestions} from '../../redux-features/friendship
 import { UserDetails } from "../../types/users/userType";
 import { useAppDispatch, useAppSelector } from "../../utils/redux-hooks";
 import { useEffect, useState } from 'react';
+import { fetchAllChannelsInDatabase, selectAllChannels } from '../../redux-features/chat/channelsSlice';
+import { Channel } from '../../types/chat/chatTypes';
 
-function SearchBar({content, setResults }: { content: string; setResults: React.Dispatch<React.SetStateAction<UserDetails[]>> }) {	  
+function SearchBar({content, setResults }: { content: string; setResults: React.Dispatch<React.SetStateAction<(UserDetails | Channel)[] >> }) {	  
 
 	const [input, setInput] = useState<string>('');
 
@@ -15,18 +17,25 @@ function SearchBar({content, setResults }: { content: string; setResults: React.
 
 	// when the search component is mounted the first time, get the list of users
 	useEffect(() => {dispatch(FetchAllUsers())}, []);
-
-	// const allUsers:UserDetails[] = useAppSelector(selectSuggestions);
 	const allUsers: UserDetails[] = useAppSelector((state) => selectSuggestions(state) as UserDetails[]);
-
-	// console.log("usersList from backend = ", allUsers);
 	
-	// this should be done in the backend
-	function fetchData(value:string) {
+	useEffect(() => {dispatch(fetchAllChannelsInDatabase())}, []);
+	const channels = useAppSelector((state) => selectAllChannels (state)) as Channel[];
+	
+	const usersAndChannels = [...allUsers, ...channels];
 
-		const results = allUsers.filter((user) => {
-		const name = user?.login?.toLowerCase();
-		return value && user && name && name.includes(value);
+	function fetchData(value:string) {
+		const results = usersAndChannels.filter((item) => {
+			// const name = user?.login?.toLowerCase();
+			// return value && user && name && name.includes(value);
+			if ('login' in item) {
+				const name = (item as UserDetails).login?.toLowerCase();
+				return value && name && name.includes(value);
+			} else if ('name' in item) {
+				const name = (item as Channel).name?.toLowerCase();
+				return value && name && name.includes(value);
+			}
+			return false;
 		});
 		setResults(results);
 	}
