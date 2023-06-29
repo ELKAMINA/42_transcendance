3ca5c2e6509f7c73348d9b3ba53be73d8fc46489
Alicis Marchand <alicia.mrchd@gmail.com>
2023-06-29 17:33:33 +0200

    ADD: upon click (=selection) by the user, fetching selected channel in database through server request

--- client/src/pages/chat.tsx
+++ client/src/pages/chat.tsx
@@ -1,46 +1,32 @@
-import { Provider, useDispatch, useSelector } from 'react-redux';
+import './chat.css'
+
 import Navbar from '../components/NavBar';
 import SideBar from './sideBar';
-import { RootState, store } from '../app/store';
-import { Box, Stack } from '@mui/material';
 import Conversation from '../components/Conversation/Conversation';
-import { DisplayedChat, Channel } from '../types/chat/chatTypes'
-import './chat.css'
-import { setSelectedChat } from '../redux-features/chat/selectedChatSlice';
-import { useAppDispatch, useAppSelector } from "../utils/redux-hooks";
-import React from "react";
-import { initChat } from '../redux-features/chat/chatsSlice';
-import { fetchUserChannels, selectUserChannels } from '../redux-features/chat/channelsSlice';
+
+import { Provider } from 'react-redux';
+import { store } from '../app/store';
+import { Box, Stack } from '@mui/material';
+import { useAppDispatch } from "../utils/redux-hooks";
+import { useEffect, useState } from "react";
+import { fetchDisplayedChannel } from '../redux-features/chat/channelsSlice';
 
 function Chat () {
 	const currentRoute = window.location.pathname;
-	const dispatchSimple = useDispatch();
-	const dispatch = useAppDispatch();
+	const [selectedChannel, setselectedChannel] = useState<string>('')
+	
+	const AppDispatch = useAppDispatch();
+	useEffect(() => {AppDispatch(fetchDisplayedChannel(selectedChannel))}, []);
 
-	React.useEffect(() => {dispatch(fetchUserChannels())}, []);
-	const channels = useAppSelector((state) => selectUserChannels(state)) as Channel[];
-	// console.log("user's channels : ", channels);
-
-	React.useEffect(() => {
-		dispatchSimple(initChat([...channels]));
-	}, []);
-
-	// the list of the Chats
-	const chats : DisplayedChat[] = useSelector((state: RootState) => state.persistedReducer.chats);
-
-	// define what to do when a Chat is selected
-	function handleSelectChat (chatID : string) {
-		// console.log('chatID = ', chatID);
-		const selectedChat = chats.find(chat => chat.name === chatID)
-		// console.log("selectedChat.name = ", selectedChat.name)
-		dispatchSimple(setSelectedChat(selectedChat))
+	function handleSelectChannel (channelName : string) {
+		setselectedChannel(channelName)
 	}
   
 	return (
 		<Provider store={store}>
 			<Navbar currentRoute={ currentRoute }/>
 			<Stack className='chat' direction={'row'} sx={{width: '100%'}}>
-				<SideBar handleSelectItem={handleSelectChat} />
+				<SideBar handleSelectItem={handleSelectChannel} />
 				<Box sx={{
 					height: '95vh',
 					width: '100vw',
