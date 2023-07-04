d6e31efd25d09a170e3e25b5ca7d4a8966445bfc
Alicis Marchand <alicia.mrchd@gmail.com>
2023-07-04 14:53:46 +0200

    UPDT: replaced is24hoursapart by areDifferentDays

--- client/src/utils/is24hoursapart.ts
+++
@@ -1,7 +0,0 @@
-function is24HoursApart(currentTime: Date, previousTime: Date): boolean {
-	const currentTimestamp: number = currentTime.getTime();
-	const previousTimestamp: number = previousTime.getTime();
-	const twentyFourHoursInMilliseconds: number = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
-  
-	return currentTimestamp - previousTimestamp >= twentyFourHoursInMilliseconds;
-  }
