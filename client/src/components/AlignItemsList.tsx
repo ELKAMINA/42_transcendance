fe56f3911af9f11ca8df9f5cb591bd55bba5e88e
Alicis Marchand <alicia.mrchd@gmail.com>
2023-07-05 10:41:08 +0200

    UPDT: added some css to prevent icons from overlapping item text

--- client/src/components/AlignItemsList.tsx
+++ client/src/components/AlignItemsList.tsx
@@ -5,7 +5,6 @@ import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
-import { useDispatch } from 'react-redux';
 import { IconButton, ListItemButton, Stack, Tooltip } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/DeleteOutline';
 import { SensorDoor } from '@mui/icons-material';
@@ -22,92 +21,101 @@ interface alignItemsProps {
   getSelectedItem: getSelectedItemFunction;
 }
 
-export default function AlignItemsList({getSelectedItem} : alignItemsProps) {
-	
-	const AppDispatch = useAppDispatch();
+export default function AlignItemsList({ getSelectedItem }: alignItemsProps) {
+  const [showIcons, setShowIcons] = React.useState(true);
+  const AppDispatch = useAppDispatch();
 
-	React.useEffect(() => {AppDispatch(fetchUserChannels())}, []);
-	const channels = useAppSelector((state) => selectUserChannels(state)) as Channel[];
+  React.useEffect(() => {
+    AppDispatch(fetchUserChannels());
+  }, []);
+  const channels = useAppSelector((state) => selectUserChannels(state)) as Channel[];
 
-	async function deleteChannel(channelToDelete : string) {
-		await api
-		.post('http://localhost:4001/channel/deleteChannelByName', {name : channelToDelete})
-		.then((response) => {console.log('channel deleted!'); AppDispatch(fetchUserChannels())})
-		.catch((error) => console.log('error while deleting channel', error))
-	}
+  async function deleteChannel(channelToDelete: string) {
+    await api
+      .post('http://localhost:4001/channel/deleteChannelByName', { name: channelToDelete })
+      .then((response) => {
+        console.log('channel deleted!');
+        AppDispatch(fetchUserChannels());
+      })
+      .catch((error) => console.log('error while deleting channel', error));
+  }
 
-	// this function allow to remove channels or users from the displayed list
-	// it is triggered when the user click on the DeleteButton component
-	function handleClick(channelToDelete : string): void {
-		deleteChannel(channelToDelete)
-	}
+  function handleClick(channelToDelete: string): void {
+    deleteChannel(channelToDelete);
+  }
 
-	const [selectedIndex, setSelectedIndex] = React.useState(() => {
-		// Retrieve the stored index value from localStorage
-		const storedIndex = localStorage.getItem('selectedItemIndex');
-		return storedIndex !== null ? Number(storedIndex) : 0;
-	});
+  const [selectedIndex, setSelectedIndex] = React.useState(() => {
+    const storedIndex = localStorage.getItem('selectedItemIndex');
+    return storedIndex !== null ? Number(storedIndex) : 0;
+  });
 
-	const handleListItemClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, index: number, ) => {
-		setSelectedIndex(index);
-		localStorage.setItem('selectedItemIndex', String(index)); // Store the selected index in localStorage
-		getSelectedItem(channels[index].name)
-	};
+  const handleListItemClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, index: number) => {
+    setSelectedIndex(index);
+    localStorage.setItem('selectedItemIndex', String(index));
+    getSelectedItem(channels[index].name);
+  };
 
-	// --------------------------------------------------------------------------------
-	return (
-		<List sx={{ width: '100%', bgcolor: 'transparent', color: 'white' }}>
-			{channels.map((element, index) => (
-				<Stack key={index}>
-					<Divider variant="inset" component="li" />
-					<ListItemButton
-						selected={selectedIndex === index}
-						onClick={(event) => handleListItemClick(event, index)}
-						sx={{
-							'&:hover': { backgroundColor: 'rgba(116, 131, 145, 0.4)' },
-							'&.Mui-selected': { backgroundColor: '#032B50' }
-	
-						}}
-					>
-					<ListItem
-						alignItems="center"
-						secondaryAction={
-							<Stack onClick={() => handleClick(element.name)} direction={'row'} spacing={5} alignItems={'end'}>
-								
-								{element.type === 'private' && (
-									 <Tooltip title="private channel" placement="top">
-										<SensorDoor/>
-									 </Tooltip>
-								)}
-								{element.type === 'public' && (
-									 <Tooltip title="public channel" placement="top">
-										<Diversity3Icon />
-									 </Tooltip>
-								)}
-								{element.protected_by_password === true && (
-									 <Tooltip title="protected by password" placement="top">
-										<LockIcon />
-									 </Tooltip>
-								)}
-								<IconButton aria-label="delete" sx={{ p: 0 }}>
-									 <Tooltip title="delete chat" placement="top">
-										 <DeleteIcon sx={{color:'red'}} fontSize='small'/>
-									 </Tooltip>
-								</IconButton>
-							</Stack>
-						}
-					>
-						<ListItemAvatar>
-							<Avatar alt={element.name} src={element.avatar} />
-						</ListItemAvatar>
-						<ListItemText
-							sx= {{color:'white'}}
-							primary={element.name}
-						/>
-					</ListItem>
-					</ListItemButton>
-				</Stack>
-			))}
-		</List>
-		);
+  const handleWindowResize = () => {
+    if (window.innerWidth < 600) {
+      setShowIcons(false);
+    } else {
+      setShowIcons(true);
+    }
+  };
+
+  React.useEffect(() => {
+    window.addEventListener('resize', handleWindowResize);
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
+
+  return (
+    <List sx={{ width: '100%', bgcolor: 'transparent', color: 'white' }}>
+      {channels.map((element, index) => (
+        <Stack key={index}>
+          <Divider variant="inset" component="li" />
+          <ListItemButton
+            selected={selectedIndex === index}
+            onClick={(event) => handleListItemClick(event, index)}
+            sx={{
+              '&:hover': { backgroundColor: 'rgba(116, 131, 145, 0.4)' },
+              '&.Mui-selected': { backgroundColor: '#032B50' }
+            }}
+          >
+            <ListItem alignItems="center">
+              <ListItemAvatar>
+                <Avatar alt={element.name} src={element.avatar} />
+              </ListItemAvatar>
+              <Stack direction="row" alignItems="center">
+                {showIcons && element.type === 'private' && (
+                  <Tooltip title="private channel" placement="top">
+                    <SensorDoor />
+                  </Tooltip>
+                )}
+                {showIcons && element.type === 'public' && (
+                  <Tooltip title="public channel" placement="top">
+                    <Diversity3Icon />
+                  </Tooltip>
+                )}
+                {showIcons && element.protected_by_password === true && (
+                  <Tooltip title="protected by password" placement="top">
+                    <LockIcon />
+                  </Tooltip>
+                )}
+              </Stack>
+              <ListItemText sx={{ flexGrow: 1, marginLeft: showIcons ? 1 : 0 }} primary={element.name} />
+              {showIcons && (
+                <IconButton aria-label="delete" sx={{ p: 0 }}>
+                  <Tooltip title="delete chat" placement="top">
+                    <DeleteIcon sx={{ color: 'red' }} fontSize="small" />
+                  </Tooltip>
+                </IconButton>
+              )}
+            </ListItem>
+          </ListItemButton>
+        </Stack>
+      ))}
+    </List>
+  );
 }
