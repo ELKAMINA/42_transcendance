69e884df713ae45eab8f71d45632c2876b77729f
Alicis Marchand <alicia.mrchd@gmail.com>
2023-07-10 17:41:39 +0200

    UPDT: createdBy property in Channel is now a user and no more a string

--- client/src/data/emptyChannel.ts
+++ client/src/data/emptyChannel.ts
@@ -2,4 +2,6 @@ import { Channel } from "../types/chat/channelTypes";
 
 export const emptyChannel : Channel = {
 	name : 'empty channel',
+	createdBy : {login : 'casper_le_fantome', displayName : 'casper_le_fantome', email: 'casper@fantome.soul', avatar : 'white_sheet'},
+	members : []
 }
