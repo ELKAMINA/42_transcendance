a5945a684a7af6de328c401d739810fbbe82afc7
Alicis Marchand <alicia.mrchd@gmail.com>
2023-07-13 18:24:02 +0200

    ADD: tests for all the new channel fetchers

--- client/src/pages/search/searchBar.tsx
+++ client/src/pages/search/searchBar.tsx
@@ -6,7 +6,7 @@ import { FetchAllFriends, FetchAllUsers, selectFriends, selectSuggestions} from 
 import { UserDetails } from "../../types/users/userType";
 import { useAppDispatch, useAppSelector } from "../../utils/redux-hooks";
 import { useEffect, useState } from 'react';
-import { fetchAllChannelsInDatabase, selectAllChannels } from '../../redux-features/chat/channelsSlice';
+import { fetchAllChannelsInDatabase, fetchPrivateChannels, fetchPrivateConvs, fetchPublicChannels, fetchUserPrivateChannels, fetchUserPrivateConvs, fetchUserPublicChannels, selectAllChannels, selectPrivateChannels, selectPrivateConvs, selectPublicChannels, selectUserPrivateChannels, selectUserPrivateConvs, selectUserPublicChannels } from '../../redux-features/chat/channelsSlice';
 import { Channel } from '../../types/chat/channelTypes';
 import { selectCurrentUser } from '../../redux-features/auth/authSlice';
 import { FetchUserByName } from '../../utils/global/global';
@@ -20,11 +20,37 @@ function SearchBar({content, setResults }: { content: string; setResults: React.
 
 	useEffect(() => {dispatch(FetchAllFriends())}, [dispatch]); // get the friends
 	const friends = useAppSelector(selectFriends) as UserDetails[];
-	// console.log('friends = ', friends);
 
 	useEffect(() => {dispatch(fetchAllChannelsInDatabase())}, []); // get the channels
 	let channels = useAppSelector((state) => selectAllChannels (state)) as Channel[];
 	
+
+	// FOR TEST PURPOSES
+	// useEffect(() => {dispatch(fetchUserPrivateChannels())}, []); // get the channels
+	// let userPrivateChannels = useAppSelector(selectUserPrivateChannels) as Channel[];
+	// console.log(userPrivateChannels);
+
+	// useEffect(() => {dispatch(fetchUserPublicChannels())}, []); // get the channels
+	// let userPublicChannels = useAppSelector(selectUserPublicChannels) as Channel[];
+	// console.log(userPublicChannels);
+
+	// useEffect(() => {dispatch(fetchUserPrivateConvs())}, []); // get the channels
+	// let userPrivateConvs = useAppSelector(selectUserPrivateConvs) as Channel[];
+	// console.log('userPrivateConvs = ', userPrivateConvs);
+
+	// useEffect(() => {dispatch(fetchPrivateChannels())}, []); // get the channels
+	// let PrivateChannels = useAppSelector(selectPrivateChannels) as Channel[];
+	// console.log('PrivateChannels = ', PrivateChannels);
+
+	// useEffect(() => {dispatch(fetchPublicChannels())}, []); // get the channels
+	// let PublicChannels = useAppSelector(selectPublicChannels) as Channel[];
+	// console.log('PublicChannels = ' , PublicChannels);
+
+	// useEffect(() => {dispatch(fetchPrivateConvs())}, []); // get the channels
+	// let PrivateConvs = useAppSelector(selectPrivateConvs) as Channel[];
+	// console.log('PublicChannels = ' , PrivateConvs);
+
+
 	// if the channel is of type 'private'or 'privateConv' and the current user is not a member,
 	// we won't display it.
 	// so I filter all the private channels and privateConv channels of which I am not a member or
