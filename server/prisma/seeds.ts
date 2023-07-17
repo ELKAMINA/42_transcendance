738df676942a50206326a6e04cf70bb97a8f6c94
Alicia Marchand <alicia.mrchd@gmail.com>
2023-07-17 10:37:20 +0200

    ADD: some users and a channel

--- server/prisma/seeds.ts
+++ server/prisma/seeds.ts
@@ -78,6 +78,103 @@ async function main() {
     },
   });
 
+  const cersei_lannister = await prisma.user.upsert({
+	where: { login: 'cersei_lannister' },
+	update: {},
+	create: {
+		login: 'cersei_lannister',
+		hash: await argon.hash('cersei_lannister'),
+		status: 'Playing',
+		fA: 'more wine!',
+		faEnabled: true,
+		rank: 1,
+		level: 950,
+		totalloss: 10,
+		totalWins: 150,
+		totalMatches: 160,
+		// friends: {
+			// connect: [ 
+				// {login : 'arya_stark'},
+				// {login : 'jamie_lannister'},
+				// {login : 'jon_snow'},
+			// ]
+		// },
+    },
+  });
+
+  const arya_stark = await prisma.user.upsert({
+	where: { login: 'arya_stark' },
+	update: {},
+	create: {
+		login: 'arya_stark',
+		hash: await argon.hash('arya_stark'),
+		status: 'Playing',
+		fA: 'salutcava',
+		faEnabled: true,
+		rank: 1,
+		level: 950,
+		totalloss: 10,
+		totalWins: 150,
+		totalMatches: 160,
+		// friends: {
+			// connect: [ 
+				// {login : 'cersei_lannister'},
+				// {login : 'jamie_lannister'},
+				// {login : 'jon_snow'},
+			// ]
+		// },
+	},
+  });
+
+  const jamie_lannister = await prisma.user.upsert({
+    where: { login: 'jamie_lannister' },
+    update: {},
+    create: {
+      login: 'jamie_lannister',
+      hash: await argon.hash('jamie_lannister'),
+      status: 'Playing',
+      fA: 'salutcava',
+      faEnabled: true,
+      rank: 1,
+      level: 950,
+      totalloss: 10,
+      totalWins: 150,
+      totalMatches: 160,
+	//   friends: {
+		// connect: [ 
+			// {login : 'cersei_lannister'},
+			// {login : 'arya_stark'},
+			// {login : 'jon_snow'},
+		// ]
+		// },
+    },
+  });
+
+  const jon_snow = await prisma.user.upsert({
+	where: { login: 'jon_snow' },
+	update: {},
+	create: {
+		login: 'jon_snow',
+		hash: await argon.hash('jon_snow'),
+		status: 'Playing',
+		fA: 'salutcava',
+		faEnabled: true,
+		rank: 1,
+		level: 950,
+		totalloss: 10,
+		totalWins: 150,
+		totalMatches: 160,
+		// friends: {
+			// connect: [ 
+				// {login : 'cersei_lannister'},
+				// {login : 'arya_stark'},
+				// {login : 'jamie_lannister'},
+			// ]
+		// },
+	},
+		
+  });
+
   const globalInfo = await prisma.globalInformation.upsert({
     where: { pid: '1' },
     update: {},
@@ -92,6 +189,23 @@ async function main() {
     create: {
       name: 'WelcomeChannel',
       members: {connect: {login: 'CasperLeFantome'}},
+      admins: {connect: {login: 'CasperLeFantome'}},
+      type: 'public',
+    },
+  });
+
+  const Breakfast_in_Westeros = await prisma.channel.upsert({
+    where: { name: 'Breakfast in Westeros' },
+    update: {},
+    create: {
+      name: 'Breakfast in Westeros',
+	  members: {
+		connect: [
+		  { login: 'arya_stark' },
+		  { login: 'cersei_lannister' },
+		  { login: 'jamie_lannister' },
+		],
+	  },
       admins: {connect: {login: 'CasperLeFantome'}},
       type: 'public',
     },
