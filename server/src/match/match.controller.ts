8f7fc4a4a7052aea730bf504759845e3b8b44f93
BarrierAntho <barrier.antho@gmail.com>
2023-07-18 17:32:23 +0200

    UPDT - [match] declare useful method
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modifié :         match/match.controller.ts

--- server/src/match/match.controller.ts
+++ server/src/match/match.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, UseGuards, Get } from '@nestjs/common';
+import { Controller, UseGuards, Get, Post } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserService } from 'src/user/user.service';
 import { AuthGuard } from '@nestjs/passport';
@@ -11,5 +11,35 @@ export class MatchController {
   @UseGuards(AuthGuard('jwt'))
   getLeaderboard() {
     return "I'm the leaderboard";
+  }
+
+  /*** GET ALL USER FROM THE QUEUE ***/
+  @Get('/all')
+  getAll() {
+    return;
+  }
+
+  /*** ADD A USER TO THE QUEUE ***/
+  @Post('/add')
+  addUser() {
+    return;
+  }
+
+  /*** REMOVE A PLAYER TO THE QUEUE ***/
+  @Post('/remove')
+  removeUser() {
+    return;
+  }
+
+  /** FIND AN OPPONENT FROM THE QUEUE ***/
+  @Get('/match')
+  findUser() {
+    return;
+  }
+
+  /*** UPDATE USER STATUS TO `IN GAME` ***/
+  @Post('/update')
+  updateStateUser() {
+    return;
   }
 }
