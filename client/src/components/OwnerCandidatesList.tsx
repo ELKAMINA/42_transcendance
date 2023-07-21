eaad68f1d9d23ea4665e24ad6d6424622f34606c
Alicis Marchand <alicia.mrchd@gmail.com>
2023-07-21 11:59:12 +0200

    ADD: OwnerCandidatesList component

---
+++ client/src/components/OwnerCandidatesList.tsx
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Checkbox from '@mui/material/Checkbox';
+import Avatar from '@mui/material/Avatar';
+import { UserModel } from '../types/users/userType';
+import { useAppSelector } from '../utils/redux-hooks';
+import { Channel, ChannelModel } from '../types/chat/channelTypes';
+import { selectDisplayedChannel } from '../redux-features/chat/channelsSlice';
+
+type OwnerCandidatesListProps = {
+	setUpdatedOwner : (owner : UserModel | null) => void,
+}
+
+export default function OwnerCandidatesList({setUpdatedOwner} : OwnerCandidatesListProps) {
+	const selectedChannel : ChannelModel = useAppSelector((state) => selectDisplayedChannel(state));
+	const channelMembers : UserModel[] = selectedChannel.members;
+
+	const ownerIndex : number = channelMembers.findIndex(member => selectedChannel.ownedById === member.login);
+
+	const [checked, setChecked] = React.useState<number>(ownerIndex);
+
+	const handleToggle = (value: number) => () => {
+		
+		const currentIndex = value;
+		let newChecked = checked;
+		
+		console.log('value = ', value);
+		console.log('newChecked = ', newChecked);
+		console.log('currentIndex = ', currentIndex);
+
+		if (currentIndex === checked) {
+			// If the clicked checkbox is already checked, uncheck it by setting newChecked to -1
+			newChecked = -1;
+		  } else {
+			// If the clicked checkbox is not checked, check it by setting newChecked to its value
+			newChecked = value;
+		  }
+
+    	setChecked(newChecked);
+		const updatedOwner: UserModel | null = newChecked !== -1 ? channelMembers[newChecked] : null;
+		setUpdatedOwner(updatedOwner);
+  	};
+
+  return (
+    <List dense sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      {channelMembers.map((el, value) => {
+        const labelId = `checkbox-list-secondary-label-${value}`;
+        return (
+          <ListItem
+            key={value}
+            secondaryAction={
+              <Checkbox
+                edge="end"
+                onChange={handleToggle(value)}
+                checked={value === checked}
+                inputProps={{ 'aria-labelledby': labelId }}
+              />
+            }
+            disablePadding
+          >
+            <ListItemButton>
+              <ListItemAvatar>
+                <Avatar
+                  alt={`Avatar of ${el.login}`}
+                  src={el?.avatar}
+                />
+              </ListItemAvatar>
+              <ListItemText id={labelId} primary={el.login} />
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
