250be8a70c5d9d442fe435bd97b1d7737c1c7731
Alicis Marchand <alicia.mrchd@gmail.com>
2023-07-24 16:59:56 +0200

    ADD: responsive date picker

---
+++ client/src/components/ResponsiveTimePicker.tsx
@@ -0,0 +1,57 @@
+import dayjs from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import { renderTimeViewClock } from '@mui/x-date-pickers/timeViewRenderers';
+
+import { Box } from '@mui/material';
+
+export type ResponsiveTimePickerProps = {
+	handleSelectedTime : (time : string) => void;
+	label : string
+}
+
+export default function ResponsiveTimePicker({handleSelectedTime, label} : ResponsiveTimePickerProps) {
+
+	function handleChange(value : any) {
+		// Convert the 'value' object to a dayjs instance
+		const selectedTime = dayjs(value);
+
+		// Extract the hour and minute from the selected time
+		// const hour = selectedTime.hour();
+		// const minute = selectedTime.minute();
+	
+		// Format the time as HH:mm (24-hour format)
+		const formattedTime = selectedTime.format('HH:mm');
+		// console.log('formattedTime', formattedTime)
+		handleSelectedTime(formattedTime);
+	}
+
+	return (
+		<LocalizationProvider dateAdapter={AdapterDayjs}>
+			<Box sx={{backgroundColor: 'transparent', marginLeft: '30%', marginRight: '3%'}}>
+				<TimePicker
+					defaultValue={dayjs('2022-04-17T15:30')}
+					onChange={handleChange}
+					label={label}
+					viewRenderers={{
+						hours: renderTimeViewClock,
+						minutes: renderTimeViewClock,
+						seconds: renderTimeViewClock,
+					}}
+					sx={{
+						width: '100%',
+						PaddingRight: '0',
+						backgroundColor: 'transparent',
+						'& .MuiInputBase-input': { // This targets the input text
+							color: '#021a30',
+							backgroundColor: 'transparent',
+							boxShadow: 'none',
+							padding: '8px',
+						},
+					}}
+				/>
+			</Box>
+		</LocalizationProvider>
+	);
+}
