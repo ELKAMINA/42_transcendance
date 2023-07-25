1481519975018d2092a9c8cc736eb5c3746690c7
Alicis Marchand <alicia.mrchd@gmail.com>
2023-07-25 18:26:04 +0200

    ADD: BannedSchedulerService to the list of modules
    git this beach 🌴
    ---------------------------------------------------------------------------
    
    Files being committed:
    server/src/channel/channel.module.ts

--- server/src/channel/channel.module.ts
+++ server/src/channel/channel.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ChannelService } from './channel.service';
 import { channelController } from './channel.controller';
 import { MuteSchedulerService } from './MuteSchedulerService';
+import { BannedSchedulerService } from './BannedSchedulerService';
 
 @Module({
 	imports: [], 
-	providers: [ChannelService, MuteSchedulerService],
+	providers: [ChannelService, MuteSchedulerService, BannedSchedulerService],
 	controllers: [channelController],
 })
 export default class channelModule {}
 export class MuteSchedulerModule {}
+export class BannedSchedulerModule {}
