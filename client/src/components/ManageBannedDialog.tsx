a044ece5f0f736f454be87abceea72e7f2dbb786
Alicis Marchand <alicia.mrchd@gmail.com>
2023-07-25 18:15:38 +0200

    UPDT: can ban user with a timer
    git this beach 🌴
    ---------------------------------------------------------------------------
    
    Files being committed:
    client/src/components/ManageBannedDialog.tsx

--- client/src/components/ManageBannedDialog.tsx
+++ client/src/components/ManageBannedDialog.tsx
@@ -7,12 +7,12 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
-import UserList from './UserList';
+import UserList, { UserWithTime } from './UserList';
 import { useAppDispatch, useAppSelector } from '../utils/redux-hooks';
 import { fetchDisplayedChannel, fetchUserChannels, selectDisplayedChannel } from '../redux-features/chat/channelsSlice';
 import api from '../utils/Axios-config/Axios';
-import { Channel, ChannelModel } from '../types/chat/channelTypes';
-import { UserByLogin, UserModel } from '../types/users/userType';
+import { ChannelModel } from '../types/chat/channelTypes';
+import { UserModel } from '../types/users/userType';
 import SendIcon from '@mui/icons-material/Send';
 
 export default function ManageBannedDialog({openDialog, setOpenDialog} : {openDialog : boolean, setOpenDialog : (arg0 : boolean) => void}) {
@@ -20,13 +20,37 @@ export default function ManageBannedDialog({openDialog, setOpenDialog} : {openDi
 	const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
 	const selectedChannel : ChannelModel = useAppSelector((state) => selectDisplayedChannel(state));
 	const [updatedBanned, setUpdatedBanned] = React.useState<UserModel[]>([]);
+	const [updatedBannedWithTime, setUpdatedBannedWithTime] = React.useState<UserWithTime[]>([]);
+
 	const AppDispatch = useAppDispatch();
 
+	function setBannedWithTime() {
+		// for all the users in updatedBanned with no time associated,
+		// add them to the updatedBannedWithTime with a time set to null
+		const updatedBannedWithNullTime = updatedBanned.map(user => {
+			// Check if the user already exists in updatedBannedWithTime
+			const existingUserIndex = updatedBannedWithTime.findIndex(
+			  userTime => userTime.user.login === user.login
+			);
+		
+			// If the user is not found in updatedBannedWithTime, add it with time set to null
+			if (existingUserIndex === -1) {
+				return { user: user, ExpiryTime: null };
+			}
+		
+			// If the user is found, return the existing entry
+			return updatedBannedWithTime[existingUserIndex];
+		});
+		// Update the updatedBannedWithTime state
+		setUpdatedBannedWithTime(updatedBannedWithNullTime);
+	}
+
 	async function updateBanned() : Promise<void> {
+		setBannedWithTime();
 		await api
 			.post('http://localhost:4001/channel/updateBanned', {
 				channelName : {name : selectedChannel.name},
-				banned : updatedBanned,
+				banned : updatedBannedWithTime,
 			})
 			.then((response) => {
 				// console.log("response = ", response)
@@ -63,15 +87,33 @@ export default function ManageBannedDialog({openDialog, setOpenDialog} : {openDi
 				open={openDialog}
 				onClose={handleCancel}
 				aria-labelledby="manage-banned-dialog"
+				maxWidth="sm" // Set the maxWidth to "md" for medium width
 			>
 			<DialogTitle id="manage-banned-dialog">
 				{"Manage who is banned from the channel"}
 			</DialogTitle>
-			<DialogContent>
-				<DialogContentText>
-					Here you can decide who is banned from the channel. 
+			<DialogContent
+			>
+				<DialogContentText
+					sx={{
+						// backgroundColor: 'red',
+						// width: '50%'
+						paddingBottom: '3%'
+					}}
+					
+				>
+					Here you can decide who is banned from the channel.
+					If you set a timer, the user will be banned until 
+					[time set by the timer].
+					If you don't, he will be banned until manually un-banned.
 				</DialogContentText>
-				<UserList usersSet={membersOptions} initialUsers={selectedChannel.banned} setUpdatedUsers={setUpdatedBanned}/>
+				<UserList 
+					usersSet={membersOptions} 
+					initialUsers={selectedChannel.banned} 
+					setUpdatedUsers={setUpdatedBanned}
+					setTimer={true}
+					setUpdatedUsersWithTime={setUpdatedBannedWithTime}
+				/>
 			</DialogContent>
 			<DialogActions>
 				<Button variant="contained" size='medium' endIcon={<SendIcon />} onClick={handleClose} autoFocus>
