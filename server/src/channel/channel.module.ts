2f8943dcb06a161fb7c46eeb928b752829ccecda
Alicis Marchand <alicia.mrchd@gmail.com>
2023-07-25 16:05:36 +0200

    ADD: MuteSchedulerService to the list of modules
    git this beach 🌴
    ---------------------------------------------------------------------------
    
    Files being committed:
    server/src/channel/channel.controller.ts
    
    Files being committed:
    server/src/channel/channel.module.ts

--- server/src/channel/channel.module.ts
+++ server/src/channel/channel.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ChannelService } from './channel.service';
 import { channelController } from './channel.controller';
+import { MuteSchedulerService } from './MuteSchedulerService';
 
 @Module({
 	imports: [], 
-	providers: [ChannelService],
+	providers: [ChannelService, MuteSchedulerService],
 	controllers: [channelController],
 })
 export default class channelModule {}
+export class MuteSchedulerModule {}
