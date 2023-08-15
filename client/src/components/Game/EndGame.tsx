57918bb7be181d64916b33e30b74fd1987e9e340
BarrierAntho <abarrier@student.42.fr>
2023-08-15 19:04:40 +0200

    UPDT - [Game - CSS] End game CSS
    BODY:
    N/A
    
    TODO:
    N/A
    
    	modifié :         client/src/components/Game/EndGame.tsx
    	modifié :         client/src/pages/game.css
    	modifié :         client/src/pages/game.tsx

--- client/src/components/Game/EndGame.tsx
+++ client/src/components/Game/EndGame.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@mui/material";
 import Dialog from "@mui/material/Dialog";
+import { Box, Grid, Typography, Stack } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useTheme } from "@mui/material/styles";
 import DialogTitle from "@mui/material/DialogTitle";
@@ -14,81 +15,91 @@ import { useAppSelector } from "../../utils/redux-hooks";
 import { selectCurrentUser } from "../../redux-features/auth/authSlice";
 
 const EndGame: React.FC<IPongProps> = ({ room }) => {
-    const theme = useTheme();
     const user = useAppSelector(selectCurrentUser);
-    const fullScreen = useMediaQuery(theme.breakpoints.down("lg"));
     const [open, setOpen] = useState(true);
+    const [resultMsg, setResultMsg] = useState("You win");
+    const [resultMsgColor, setResultMsgColor] = useState("#07457E"); // #07457E = blue
+    const [resultMsgShadow, setResultMsgShadow] = useState(
+        "0 0 5px #00ff51,0 0 10px #00ff51, 0 0 15px #00ff51, 0 0 20px #00ff51, 0 0 30px #00ff51, 0 0 40px #00ff51"
+    ); // "0 0 5px #0ff,0 0 10px #0ff, 0 0 15px #0ff, 0 0 20px #0ff, 0 0 30px #0ff, 0 0 40px #0ff" = blue
+    const resultMsgLoose = "You loose";
+    const resultMsgLooseColor = "#07457E";
+    const resultMsgLooseShadow =
+        "0 0 5px #ff0000,0 0 10px #ff0000, 0 0 15px #ff0000, 0 0 20px #ff0000, 0 0 30px #ff0000, 0 0 40px #ff0000";
     const currentRoute = window.location.pathname;
     const navigate = useNavigate();
-
-    const handleCancel = () => {
-        setOpen(false);
-    };
 
     const goHome = () => {
         navigate("/welcome");
     };
 
+    useEffect(() => {
+        switch (user) {
+            case room.players[0]:
+                if (room.scorePlayers[0] < room.scorePlayers[1]) {
+                    setResultMsg(resultMsgLoose);
+                    setResultMsgColor(resultMsgLooseColor);
+                    setResultMsgShadow(resultMsgLooseShadow);
+                }
+                break;
+            default:
+                if (room.scorePlayers[1] < room.scorePlayers[0]) {
+                    setResultMsg(resultMsgLoose);
+                    setResultMsgColor(resultMsgLooseColor);
+                    setResultMsgShadow(resultMsgLooseShadow);
+                }
+        }
+    }, []);
+
     return (
-        <div>
+        <>
             <Navbar currentRoute={currentRoute} />
-            <Dialog fullScreen={fullScreen} open={open} onClose={handleCancel}>
-                {user === room.players[0] &&
-                    room.scorePlayers[0] > room.scorePlayers[1] && (
-                        <>
-                            <DialogTitle>You win </DialogTitle>
-                            <DialogContent>
-                                {" "}
-                                Score : {room.scorePlayers[0]} :{" "}
-                                {room.scorePlayers[1]}
-                            </DialogContent>
-                        </>
-                    )}
-                {user === room.players[0] &&
-                    room.scorePlayers[0] < room.scorePlayers[1] && (
-                        <>
-                            <DialogTitle>You Lose </DialogTitle>
-                            <DialogContent>
-                                {" "}
-                                Score : {room.scorePlayers[0]} :{" "}
-                                {room.scorePlayers[1]}
-                            </DialogContent>
-                        </>
-                    )}
-                {user === room.players[1] &&
-                    room.scorePlayers[0] > room.scorePlayers[1] && (
-                        <>
-                            <DialogTitle>You lose </DialogTitle>
-                            <DialogContent>
-                                {" "}
-                                Score : {room.scorePlayers[0]} :{" "}
-                                {room.scorePlayers[1]}
-                            </DialogContent>
-                        </>
-                    )}
-                {user === room.players[1] &&
-                    room.scorePlayers[0] < room.scorePlayers[1] && (
-                        <>
-                            <DialogTitle>You Win </DialogTitle>
-                            <DialogContent>
-                                {" "}
-                                Score : {room.scorePlayers[0]} :{" "}
-                                {room.scorePlayers[1]}
-                            </DialogContent>
-                        </>
-                    )}
-            </Dialog>
-            <DialogActions>
-                <Button
-                    variant="contained"
-                    size="medium"
-                    autoFocus
-                    onClick={goHome}
+            <Box
+                sx={{
+                    height: "100vh",
+                    alignItems: "center",
+                    background:
+                        "linear-gradient(180deg, #07457E 0%, rgba(0, 181, 160, 0.69) 97%)",
+                }}
+            >
+                <Dialog
+                    fullScreen={false}
+                    maxWidth={"sm"}
+                    open={open}
+                    onClose={goHome}
                 >
-                    Back home
-                </Button>
-            </DialogActions>
-        </div>
+                    <DialogTitle>
+                        <Typography
+                            align="center"
+                            sx={{
+                                fontSize: 50,
+                                margin: 1,
+                                color: resultMsgColor,
+                                textShadow: resultMsgShadow,
+                                transition: "all 0.3s ease",
+                            }}
+                        >
+                            {resultMsg}
+                        </Typography>
+                    </DialogTitle>
+                    <Stack>
+                        <DialogContent>
+                            {room.scorePlayers[0]} : {room.scorePlayers[1]}
+                        </DialogContent>
+                        <DialogActions>
+                            <Button
+                                variant="contained"
+                                size="medium"
+                                autoFocus
+                                onClick={goHome}
+                            >
+                                Go to Welcome Page
+                            </Button>
+                        </DialogActions>
+                    </Stack>
+                </Dialog>
+            </Box>
+        </>
     );
 };
 
