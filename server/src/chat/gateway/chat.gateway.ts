aee71787df6494e9465c88610d09427e33112d57
Alicis Marchand <alicia.mrchd@gmail.com>
2023-08-16 15:21:58 +0200

    FIX : solving merge conflicts went wrong
    git this beach 🌴
    ---------------------------------------------------------------------------
    
    Files being committed:
    server/src/chat/gateway/chat.gateway.ts

--- server/src/chat/gateway/chat.gateway.ts
+++ server/src/chat/gateway/chat.gateway.ts
@@ -15,120 +15,106 @@ import { FriendshipService } from '../../friendship/friendship.service';
 
 @WebSocketGateway(4002, { cors: '*' }) // we want every front and client to be able to connect with our gateway
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
-  @WebSocketServer() server: Server;
+	@WebSocketServer() server: Server;
 
-  constructor(
-    private ChatService: ChatService,
-    private friends: FriendshipService,
-  ) {}
+	constructor(
+		private ChatService: ChatService,
+		private friends: FriendshipService,
+	) {}
 
-  private logger: Logger = new Logger('ChatGateway');
+	private logger: Logger = new Logger('ChatGateway');
 
-  afterInit(server: any) {
-    this.logger.log('Initialized!');
-  }
-
-  @SubscribeMessage('ChatToServer')
-  handleNewChatMessage(socket: Socket, dto: MessageDto): void {
-    // console.log('Depuis chat Servver ', socket);
-    const roomId = socket.handshake.query.roomId as string;
-    this.ChatService.createMessage(dto);
-    this.server.to(roomId).emit('ServerToChat:' + roomId, dto);
-  }
-
-	@SubscribeMessage('NotifNewPrivateConv')
-	handleNewPrivateConvCreated(socket: Socket, dto: MessageDto): void {
-	  const roomId = socket.handshake.query.roomId as string;
-	  this.ChatService.createMessage(dto);
-	  this.server.to(roomId).emit('NotifNewPrivateConv:' + roomId, dto);
+	afterInit(server: any) {
+		this.logger.log('Initialized!');
 	}
 
-	@SubscribeMessage('LeavingChannel')
-	handleUserLeavingChannel(socket: Socket, dto: MessageDto): void {
-	  const roomId = socket.handshake.query.roomId as string;
-	  
-	  this.ChatService.createMessage(dto);
-	  this.server.to(roomId).emit('ServerToChat:' + roomId, dto);
+	@SubscribeMessage('ChatToServer')
+	handleNewChatMessage(socket: Socket, dto: MessageDto): void {
+		// console.log('Depuis chat Servver ', socket);
+		const roomId = socket.handshake.query.roomId as string;
+		this.ChatService.createMessage(dto);
+		this.server.to(roomId).emit('ServerToChat:' + roomId, dto);
 	}
-  
+
+		@SubscribeMessage('NotifNewPrivateConv')
+		handleNewPrivateConvCreated(socket: Socket, dto: MessageDto): void {
+		const roomId = socket.handshake.query.roomId as string;
+		this.ChatService.createMessage(dto);
+		this.server.to(roomId).emit('NotifNewPrivateConv:' + roomId, dto);
+		}
+
+		@SubscribeMessage('LeavingChannel')
+		handleUserLeavingChannel(socket: Socket, dto: MessageDto): void {
+		const roomId = socket.handshake.query.roomId as string;
+		
+		this.ChatService.createMessage(dto);
+		this.server.to(roomId).emit('ServerToChat:' + roomId, dto);
+		}
+
+
 	@SubscribeMessage('blockUser')
-	async handleBlockUser(socket: Socket, @MessageBody() body: any,
+	async handleBlockUser(
+		socket: Socket,
+		@MessageBody() body: any,
 	): Promise<void> {
 		// const roomId = socket.handshake.query.roomId as string;
-		const user = await this.friends.blockFriend(
-			body.sender,
-			body.receiver,
-		  );
+		const user = await this.friends.blockFriend(body.sender, body.receiver);
 		//   this.server.to(roomId).emit('FriendBlocked', user)
 	}
 
-    this.ChatService.createMessage(dto);
-    this.server.to(roomId).emit('ServerToChat:' + roomId, dto);
-  }
+	@SubscribeMessage('suggestingGame')
+	async handleSuggestingGame(
+		@ConnectedSocket() socket: Socket,
+		@MessageBody() body: any,
+	): Promise<void> {
+		const roomId = socket.handshake.query.roomId as string;
+		//   this.server.to(roomId).emit('FriendBlocked', user)
+		// const [socketId, room] = socket.rooms;
+		// console.log('la room ', sockets.rooms);
+		// console.log('le body de la requete ', body);
+		this.server.to(roomId).emit('respondingGame', body);
+	}
 
-  @SubscribeMessage('blockUser')
-  async handleBlockUser(
-    socket: Socket,
-    @MessageBody() body: any,
-  ): Promise<void> {
-    // const roomId = socket.handshake.query.roomId as string;
-    const user = await this.friends.blockFriend(body.sender, body.receiver);
-    //   this.server.to(roomId).emit('FriendBlocked', user)
-  }
+	@SubscribeMessage('privateGame')
+	async handlePrivateGame(
+		@ConnectedSocket() socket: Socket,
+		@MessageBody() body: any,
+	): Promise<void> {
+		const roomId = socket.handshake.query.roomId as string;
+		//   this.server.to(roomId).emit('FriendBlocked', user)
+		// const [socketId, room] = socket.rooms;
+		// console.log('la room ', sockets.rooms);
+		// console.log('le body de la requete ', body);
+		this.server.to(roomId).emit('serverPrivateGame', body);
+	}
 
-  @SubscribeMessage('suggestingGame')
-  async handleSuggestingGame(
-    @ConnectedSocket() socket: Socket,
-    @MessageBody() body: any,
-  ): Promise<void> {
-    const roomId = socket.handshake.query.roomId as string;
-    //   this.server.to(roomId).emit('FriendBlocked', user)
-    // const [socketId, room] = socket.rooms;
-    // console.log('la room ', sockets.rooms);
-    // console.log('le body de la requete ', body);
-    this.server.to(roomId).emit('respondingGame', body);
-  }
+	@SubscribeMessage('denyGame')
+	async handleDenyGame(@ConnectedSocket() socket: Socket): Promise<void> {
+		const roomId = socket.handshake.query.roomId as string;
+		//   this.server.to(roomId).emit('FriendBlocked', user)
+		// const [socketId, room] = socket.rooms;
+		// console.log('la room ', sockets.rooms);
+		// console.log('le body de la requete ', body);
+		this.server.to(roomId).emit('gameDenied');
+	}
 
-  @SubscribeMessage('privateGame')
-  async handlePrivateGame(
-    @ConnectedSocket() socket: Socket,
-    @MessageBody() body: any,
-  ): Promise<void> {
-    const roomId = socket.handshake.query.roomId as string;
-    //   this.server.to(roomId).emit('FriendBlocked', user)
-    // const [socketId, room] = socket.rooms;
-    // console.log('la room ', sockets.rooms);
-    // console.log('le body de la requete ', body);
-    this.server.to(roomId).emit('serverPrivateGame', body);
-  }
+	@SubscribeMessage('cancelGame')
+	async handleCancelGame(@ConnectedSocket() socket: Socket): Promise<void> {
+		const roomId = socket.handshake.query.roomId as string;
+		//   this.server.to(roomId).emit('FriendBlocked', user)
+		// const [socketId, room] = socket.rooms;
+		// console.log('la room ', sockets.rooms);
+		// console.log('le body de la requete ', body);
+		this.server.to(roomId).emit('gameCancelled');
+	}
 
-  @SubscribeMessage('denyGame')
-  async handleDenyGame(@ConnectedSocket() socket: Socket): Promise<void> {
-    const roomId = socket.handshake.query.roomId as string;
-    //   this.server.to(roomId).emit('FriendBlocked', user)
-    // const [socketId, room] = socket.rooms;
-    // console.log('la room ', sockets.rooms);
-    // console.log('le body de la requete ', body);
-    this.server.to(roomId).emit('gameDenied');
-  }
+	handleConnection(socket: Socket) {
+		const roomId = socket.handshake.query.roomId as string;
+		socket.join(roomId);
+	}
 
-  @SubscribeMessage('cancelGame')
-  async handleCancelGame(@ConnectedSocket() socket: Socket): Promise<void> {
-    const roomId = socket.handshake.query.roomId as string;
-    //   this.server.to(roomId).emit('FriendBlocked', user)
-    // const [socketId, room] = socket.rooms;
-    // console.log('la room ', sockets.rooms);
-    // console.log('le body de la requete ', body);
-    this.server.to(roomId).emit('gameCancelled');
-  }
-
-  handleConnection(socket: Socket) {
-    const roomId = socket.handshake.query.roomId as string;
-    socket.join(roomId);
-  }
-
-  handleDisconnect(socket: Socket) {
-    const roomId = socket.handshake.query.roomId as string;
-    socket.leave(roomId);
-  }
+	handleDisconnect(socket: Socket) {
+		const roomId = socket.handshake.query.roomId as string;
+		socket.leave(roomId);
+	}
 }
