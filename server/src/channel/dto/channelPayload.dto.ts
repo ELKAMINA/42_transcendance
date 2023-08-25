6aeb1c274762df1485150adf5077fafe22436e46
Alicis Marchand <alicia.mrchd@gmail.com>
2023-08-25 15:13:14 +0200

    FIX: error when creating private convs due to length of the name ADD: protections to restrict channel types
    git this beach 🌴
    ---------------------------------------------------------------------------
    
    Files being committed:
    server/src/channel/dto/channelPayload.dto.ts

--- server/src/channel/dto/channelPayload.dto.ts
+++ server/src/channel/dto/channelPayload.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Message } from "@prisma/client";
-import { IsNotEmpty, IsString, IsInt, IsOptional, ValidateIf, MaxLength, MinLength } from "class-validator";
+import { IsNotEmpty, IsString, IsInt, IsOptional, ValidateIf, MaxLength, MinLength, IsEmpty, IsIn } from "class-validator";
 import { UserByLogin } from "../../user/types";
 
 export class ChannelDto {
@@ -8,21 +8,25 @@ export class ChannelDto {
 	@IsNotEmpty()
 	@ApiProperty()
 	@MinLength(1)
-	@MaxLength(20)
+	@ValidateIf((obj) => obj.type !== 'privateConv') // When type is not equal to 'PrivateConv', the @MaxLength(20) validation will be performed on the name property.
+    @MaxLength(20, { message: 'Name length should not exceed 20 characters' })
+	@ValidateIf((obj) => obj.type === 'privateConv')
+  	@MaxLength(50, { message: 'Name length should not exceed 50 characters for privateConv' })
 	name: string;
   
 	@IsString()
 	@ApiProperty()
+	@IsIn(['public', 'private', 'privateConv'], { message: 'Type must be public, private, or privateConv' })
 	type: string;
 
 	@ApiProperty()
 	protected_by_password: boolean;
 
 	@IsString()
-	@IsNotEmpty()
 	@ApiProperty()
-	// @IsOptional()
 	@ValidateIf((obj) => obj.protected_by_password === true) // if true, the password property will be validated as mandatory using the @IsNotEmpty() decorator. 
+	@IsNotEmpty({ message: 'Key must not be empty when protected_by_password is true' })
+	@IsEmpty({ message: 'Key must be empty when protected_by_password is false' })
 	key: string;
 
 	// @IsNotEmpty()
