39c7d28188adbeb3eaa80c6cdee5323864295ef5
elkamina@yahoo.com <ael-khat@paul-f5Ar2s2.clusters.42paris.fr>
2023-08-30 14:48:35 +0200

    DOCKER FOR PRODUCTION

--- server/start.prod.sh
+++ server/start.prod.sh
@@ -2,8 +2,10 @@
 
 # if [ ! -d "/app/prisma/migrations" ] #checks whether a given file exist and is a directory
 # then
-	npm run prisma:dev:deploy && # One & means runs in the background
-	node dist/src/main.js # && it executes only if cp -r worked 
+	# npm run prisma:dev:deploy && # One & means runs in the background
+	npx prisma migrate deploy 
+	npx prisma generate
+	node dist/src/main.js 
 # else
 # 	npx prisma generate &
 # 	node dist/src/main.js
